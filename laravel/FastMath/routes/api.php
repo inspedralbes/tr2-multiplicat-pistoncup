35e303c642dffbe631c5624b23d8a45bee1e1d53
a21aledelfel <a21aledlfel@inspedralbes.cat>
2023-11-29 11:48:31 +0100

    playersapi

--- laravel/FastMath/routes/api.php
+++ laravel/FastMath/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PreguntesController;
+use App\Http\Controllers\PlayersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,12 @@ Route::post('/preguntes', [PreguntesController::class, 'store']);
 Route::get('/preguntes/{id}', [PreguntesController::class, 'show']);
 Route::put('/preguntes/{id}', [PreguntesController::class, 'update']);
 Route::delete('/preguntes/{id}', [PreguntesController::class, 'destroy']);
+
+Route::get('/players', [PlayersController::class, 'index']);
+Route::post('/players', [PlayersController::class, 'store']);
+Route::get('/players/{id}', [PlayersController::class, 'show']);
+Route::put('/players/{id}', [PlayersController::class, 'update']);
+Route::delete('/players/{id}', [PlayersController::class, 'destroy']);
 
 
 
